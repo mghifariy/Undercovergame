4d08354613d37792b9b6b8cbbfa3547c4285fe07
mghifari <mohammadghifariyusuf@gmail.com>
2017-12-01 14:28:14 +0700

    commit

--- application/models/Undercovergame_m.php
+++ application/models/Undercovergame_m.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Tebakkode_m extends CI_Model {
+class Undercovergame_m extends CI_Model {
 
   function __construct(){
     parent::__construct();
